051a927fed7c4e683f0ca18a4c3ff1e61619edf0
rebecca <ungar.rivka@gmail.com>
2017-03-09 23:48:04 +0200

    finished assignment

--- app.js
+++ app.js
@@ -1,31 +1,75 @@
 (function () {
   'use strict';
 
-  angular.module( 'LunchCheck', [] )
-  .controller( 'LunchCheckController', LunchCheckController);
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  LunchCheckController.$injector = ['scope'];
-  function LunchCheckController( $scope ){
-    $scope.menu;
-    $scope.outputMessage;
-    $scope.checkMenu = function () {
-      var menu;
-      var numOfItems;
-      menu = $scope.menu
+  ToBuyController.$injector = ['ShoppingListCheckOffService'];
+  function ToBuyController( ShoppingListCheckOffService ){
+    var showList = this;
 
-      if( menu ){
-        numOfItems = menu.split(',').length;
 
-        if( numOfItems <= 3 ){
-          $scope.outputMessage = "Enjoy!";
-        }else{
-          $scope.outputMessage = "Too much!";
-        }
+    showList.items = ShoppingListCheckOffService.getToBuyList();
 
-      }else{
-        $scope.outputMessage = "Please enter data first";
-      }
+    showList.transferItemToBoughtList = function (itemIndex, name, quantity) {
+      ShoppingListCheckOffService.transferItemToBoughtList(itemIndex, name, quantity);
     }
+  }
+
+  AlreadyBoughtController.$injector = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController( ShoppingListCheckOffService ){
+    var boughtList = this;
+
+    boughtList.items = ShoppingListCheckOffService.getBoughtList();
+  }
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    var ToBuyList = [
+      {
+        name: "Milk",
+        quantity: "2"
+      },
+      {
+        name: "Bread",
+        quantity: "6"
+      },
+      {
+        name: "Sugar",
+        quantity: "1"
+      },
+      {
+        name: "Chocolate",
+        quantity: "10"
+      },
+      {
+        name: "Gum",
+        quantity: "2"
+      }
+    ]
+
+    var boughtList = []
+
+    service.getToBuyList = function () {
+      return ToBuyList;
+    }
+
+    service.getBoughtList = function () {
+      return boughtList;
+    }
+
+    service.transferItemToBoughtList = function (itemIndex, name, quantity) {
+      ToBuyList.splice(itemIndex, 1);
+      var item = {
+        name: name,
+        quantity: quantity
+      };
+      boughtList.push(item);
+    }
+
   }
 
 })();
